9b2830a70aba44aa57dedeab62a8148487944130
boonelbats <gonsemi@gmail.com>
2026-08-27 16:15:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,81 +6,83 @@ const channels = [
   {
     id: "america-tv",
     name: "América Televisión",
-    url: "http://m.mediastream.pe/canal4/livestream/playlist.m3u8"
-    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f1/America_Televisi%C3%B3n-logo%2C_Tv_port%C3%A1til.svg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=originalg",
+    group: "Perú - Nacionales",
+    url: "http://m.mediastream.pe/canal4/livestream/playlist.m3u8",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f1/America_Televisi%C3%B3n-logo%2C_Tv_port%C3%A1til.svg",
     epg_id: "America.pe"
   },
   {
     id: "willax",
     name: "Willax Televisión",
-    url: "http://movil.mediastream.pe/vv_willax/livestream/playlist.m3u8"
-    logo": "https://upload.wikimedia.org/wikipedia/commons/7/73/Willax_Televisi%C3%B3n.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original",
+    group: "Perú - Nacionales",
+    url: "http://movil.mediastream.pe/vv_willax/livestream/playlist.m3u8",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/7/73/Willax_TelevisixC3%B3n.png",
     epg_id: "Willax.pe"
   },
   {
     id: "atv",
     name: "ATV Perú",
-    url: "https://alba-pe-atv-atvmas.stream.mediatiquestream.com/index.m3u8"
-    logo: "https://upload.wikimedia.org/wikipedia/commons/0/0e/ATV_logo_2020.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original",
+    group: "Perú - Nacionales",
+    url: "https://alba-pe-atv-atvmas.stream.mediatiquestream.com/index.m3u8",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/0/0e/ATV_logo_2020.png",
     epg_id: "ATV.pe"
-  }
-];
-  {
-    "id": "panamericana-tv-pe",
-    "name": "Panamericana TV",
-    "group": "Perú - Nacionales",
-    "url": "http://45.171.108.253:8888/PANAMERICANA/index.m3u8",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/2/26/Panamericana_tv_2009.png",
-    "epg_id": "PanamericanaTV.pe"
   },
   {
-    "id": "atv-plus-noticias-pe",
-    "name": "ATV+ Noticias",
-    "group": "Perú - Noticias",
-    "url": "http://45.171.108.253:8888/ATV/index.m3u8",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/f/f4/Atv_noticias_logo.png",
-    "epg_id": "ATVPlus.pe"
+    id: "panamericana-tv-pe",
+    name: "Panamericana TV",
+    group: "Perú - Nacionales",
+    url: "http://45.171.108.253:8888/PANAMERICANA/index.m3u8",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/2/26/Panamericana_tv_2009.png",
+    epg_id: "PanamericanaTV.pe"
   },
   {
-    "id": "atv-plus-noticias-youtube",
-    "name": "ATV+ Noticias Ⓨ",
-    "group": "Perú - Noticias",
-    "url": "https://www.youtube.com/c/ATVNoticiasOficial/live",
-    "logo": "https://upload.wikimedia.org/wikipedia/commons/f/f4/Atv_noticias_logo.png",
-    "epg_id": "ATVPlus.pe",
-    "youtube": true
+    id: "atv-plus-noticias-pe",
+    name: "ATV+ Noticias",
+    group: "Perú - Noticias",
+    url: "http://45.171.108.253:8888/ATV/index.m3u8",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f4/Atv_noticias_logo.png",
+    epg_id: "ATVPlus.pe"
   },
   {
-    "id": "exitosa-pe-1",
-    "name": "Exitosa (ex-Karibeña)",
-    "group": "Perú - Noticias",
-    "url": "http://187.102.210.46/KARIBENA/index.m3u8",
-    "logo": "https://i.pinimg.com/280x280_RS/11/85/b6/1185b667fe3f80d7072359d7ce7ce52d.jpg",
-    "epg_id": "Exitosa.pe"
+    id: "atv-plus-noticias-youtube",
+    name: "ATV+ Noticias Ⓨ",
+    group: "Perú - Noticias",
+    url: "https://www.youtube.com/c/ATVNoticiasOficial/live",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/f/f4/Atv_noticias_logo.png",
+    epg_id: "ATVPlus.pe",
+    youtube: true
   },
   {
-    "id": "exitosa-pe-2",
-    "name": "Exitosa (ex-Karibeña)",
-    "group": "Perú - Noticias",
-    "url": "http://190.93.224.42/EXITOSA/index.m3u8",
-    "logo": "https://i.pinimg.com/280x280_RS/11/85/b6/1185b667fe3f80d7072359d7ce7ce52d.jpg",
-    "epg_id": "Exitosa.pe"
+    id: "exitosa-pe-1",
+    name: "Exitosa (ex-Karibeña)",
+    group: "Perú - Noticias",
+    url: "http://187.102.210.46/KARIBENA/index.m3u8",
+    logo: "https://i.pinimg.com/280x280_RS/11/85/b6/1185b667fe3f80d7072359d7ce7ce52d.jpg",
+    epg_id: "Exitosa.pe"
   },
   {
-    "id": "latina-pe",
-    "name": "Latina",
-    "group": "Perú - Nacionales",
-    "url": "https://redirector.rudo.video/hls-video/567ffde3fa319fadf3419efda25619456231dfea/latina/latina.smil/playlist.m3u8",
-    "logo": "https://graph.facebook.com/Latina.pe/picture?width=200&height=200",
-    "epg_id": "Latina.pe"
+    id: "exitosa-pe-2",
+    name: "Exitosa (ex-Karibeña)",
+    group: "Perú - Noticias",
+    url: "http://190.93.224.42/EXITOSA/index.m3u8",
+    logo: "https://i.pinimg.com/280x280_RS/11/85/b6/1185b667fe3f80d7072359d7ce7ce52d.jpg",
+    epg_id: "Exitosa.pe"
   },
   {
-    "id": "cosmos-tv-pe",
-    "name": "Cosmos TV",
-    "group": "Perú - Regionales",
-    "url": "https://videoserver.tmcreativos.com:19360/tvcosmos/tvcosmos.m3u8",
-    "logo": "https://pbs.twimg.com/profile_images/1904206504753811457/66CbqvH1_200x200.jpg",
-    "epg_id": "CosmosTV.pe"
+    id: "latina-pe",
+    name: "Latina",
+    group: "Perú - Nacionales",
+    url: "https://redirector.rudo.video/hls-video/567ffde3fa319fadf3419efda25619456231dfea/latina/latina.smil/playlist.m3u8",
+    logo: "https://graph.facebook.com/Latina.pe/picture?width=200&height=200",
+    epg_id: "Latina.pe"
+  },
+  {
+    id: "cosmos-tv-pe",
+    name: "Cosmos TV",
+    group: "Perú - Regionales",
+    url: "https://videoserver.tmcreativos.com:19360/tvcosmos/tvcosmos.m3u8",
+    logo: "https://pbs.twimg.com/profile_images/1904206504753811457/66CbqvH1_200x200.jpg",
+    epg_id: "CosmosTV.pe"
   }
 ];
 
@@ -108,7 +110,7 @@ function metaFor(c) {
     name: c.name,
     poster: c.logo,
     posterShape: "landscape",
-    description: c.epg_id ? `${c.name} — señal en vivo` : `${c.name} — señal en vivo`
+    description: `${c.name} — señal en vivo`
   };
 }
 
